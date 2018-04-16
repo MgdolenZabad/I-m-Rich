8c477c04d91a6e39a5052073c3fa0afa50e28a3a
Mgdolen <mgdolen.zabad@gmail.com>
2018-04-15 20:12:00 -0400

    Initial Commit

---
+++ I'm Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I'm Rich
+//
+//  Created by Mgdolen Zabad on 1/31/18.
+//  Copyright © 2018 MgdolenZabad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
